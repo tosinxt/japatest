9c04b948d5a1939e2bc05ac3d283c58f19cfae8b
temidayo24 <temidayo.kehinde.10@gmail.com>
2024-08-09 10:02:37 +0100

    FIX: Course Card height

--- app/components/CourseCard/index.js
+++ app/components/CourseCard/index.js
@@ -6,12 +6,12 @@ const CourseCard = ({ src, alt, title, text, path }) => {
   return (
     <Link href={path}>
       <div
-        className={`flex flex-col p-3 h-full rounded-2xl text-textDefault w-[300px] bg-primaryLight`}
+        className={`flex flex-col p-3 h-full rounded-2xl text-textDefault w-[320px] bg-primaryLight`}
       >
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 h-[400px]">
           <Image src={src} alt={alt} width={300} height={200} />
           <span className="text-lg font-bold">{title}</span>
-          <p className="text-sm w-[300px]">{text}</p>
+          <p className="text-sm w-full">{text}</p>
         </div>
         <div className="border border-textDefault w-full px-5 py-1 rounded-[30px] mt-14 mb-3 bg-transparent font-light text-center">
           Learn more
