1dfe91ab23400295451c52816cdb71e9b71675fa
temidayo24 <temidayo.kehinde.10@gmail.com>
2024-08-16 11:01:51 +0100

    modifications to rootlayout, new layout for auth pages

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -1,25 +1,22 @@
 import { Poppins } from "next/font/google";
-import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import "../globals.css";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
+
 export const metadata = {
   title: "japa",
   description:
     "Discover the ultimate platform where we upskill, empower, and walk with you to your dream career",
 };
 
-export default function RootLayout({ children }) {
+export default function AuthLayout({ children }) {
   return (
     <html lang="en">
       <body className={`relative ${poppins.className}`}>
-        <Header />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
